19bb9210d4ab2fb3a13f484b6789fa90308d6dc6
Simone Boglio <bogliosimone@gmail.com>
2015-04-21 15:48:18 +0200

    inizializzazione progetto 21-5

---
+++ bogliobresich/src/main/java/it/polimi/ingsw/bogliobresich/App.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.bogliobresich;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
