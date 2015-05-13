41d556aaf1fd65077c4fc1cda5b14aabd3d5599f
Matteo <mattebre@gmail.com>
2015-05-13 17:42:29 +0200

    Add Hand

--- bogliobresich/src/main/java/it/polimi/ingsw/bogliobresich/model/player/Hand.java
+++ bogliobresich/src/main/java/it/polimi/ingsw/bogliobresich/model/player/Hand.java
@@ -3,6 +3,10 @@
  */
 package it.polimi.ingsw.bogliobresich.model.player;
 
+import it.polimi.ingsw.bogliobresich.model.cards.Card;
+
+import java.util.ArrayList;
+
 /**
  * @author Matteo
  * @author simoneboglio
@@ -10,11 +14,52 @@ package it.polimi.ingsw.bogliobresich.model.player;
  */
 public class Hand {
 
+	ArrayList<Card> cards;
+	private int maximumNumberOfCards;
+
+	/**
+	 * Class constructor.
+	 */
+	public Hand() {
+		maximumNumberOfCards = 0;
+		cards = new ArrayList<Card>();
+	}
+
+	/**	
+	 * Return true if the hand is full
+	 */
+	public boolean isFull() {
+		if(cards.size() > this.maximumNumberOfCards)
+			return true;
+		else
+			return false;
+	}
+
+	/**
+	 * Add a card in the hand. Return false if the hand is full.
+	 */
+	public boolean addCard(Card c) {
+		return cards.add(c);
+	}
+
+	/**
+	 * Remove a card if the card is in the hand. Return false if the card is not present in the hand.
+	 */
+	public boolean removeCard(Card c) {
+		return cards.remove(c);//TODO controllare che la remove rimuova lo stesso tipo e non lo stesso oggetto
+	}
+
+	/**
+	 * Remove a card if the card is in the hand. Return false if the card is not present in the hand.
+	 */
+	public boolean searchCard(Card c) {
+		return cards.contains(c);//TODO controllare se cerca lo stesso tipo e non lo stesso oggetto
+	}
+	
 	/**
 	 * 
 	 */
-	public Hand() {
-		// TODO Auto-generated constructor stub
-	}
+	//public Card[] list(Card c) {} TODO
+
 
 }
