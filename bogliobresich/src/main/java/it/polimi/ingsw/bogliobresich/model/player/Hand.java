71748bcb0fcacba2808309f182794dba8b70ee3e
Matteo <mattebre@gmail.com>
2015-05-13 16:30:33 +0200

    Aggiunto package player e le sue classi

---
+++ bogliobresich/src/main/java/it/polimi/ingsw/bogliobresich/model/player/Hand.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package it.polimi.ingsw.bogliobresich.model.player;
+
+/**
+ * @author Matteo
+ * @author simoneboglio
+ *
+ */
+public class Hand {
+
+	/**
+	 * 
+	 */
+	public Hand() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ bogliobresich/src/main/java/it/polimi/ingsw/bogliobresich/model/player/Player.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package it.polimi.ingsw.bogliobresich.model.player;
+
+/**
+ * @author Matteo
+ * @author simoneboglio
+ * 
+ */
+public class Player {
+	
+	private boolean connected;
+	private boolean life;
+	private boolean turnEnabled;
+
+	/**
+	 * Class constructor.
+	 */
+	public Player() {
+		connected = false;
+		life = true;
+		turnEnabled = false;
+	}
+	
+	/**
+	 * Return the abilitation for a turn
+	 *   
+	 */
+	public boolean isYourTurn() {
+		return turnEnabled;
+	}
+	
+	/**
+	 * Return the connection state
+	 * 
+	 */
+	public boolean isConnected() {
+		return connected;
+	}
+	
+	/**
+	 * Return if the player is alive
+	 * 
+	 */
+	public boolean isAlive() {
+		return life;
+	}
+
+}
