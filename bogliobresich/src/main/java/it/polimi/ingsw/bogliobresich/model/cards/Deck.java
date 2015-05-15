5eecdce9ca522424f13d630c20b542494834df73
Matteo <mattebre@gmail.com>
2015-05-16 01:40:35 +0200

    Modificato Deck: aggiunti i metodi addCard e drawCard

--- bogliobresich/src/main/java/it/polimi/ingsw/bogliobresich/model/cards/Deck.java
+++ bogliobresich/src/main/java/it/polimi/ingsw/bogliobresich/model/cards/Deck.java
@@ -3,10 +3,39 @@
  */
 package it.polimi.ingsw.bogliobresich.model.cards;
 
+import java.util.ArrayList;
+
 /**
  * @author Matteo
  *
  */
 public class Deck {
+    
+    private ArrayList<Card> stackOfCards = new ArrayList<Card>();
+    
+    public Deck() {
+        
+    }
+    
+    /**
+     * Add a card to the deck 
+     * @param c Card: Card to add in the deck*/
+    public void addCard(Card c) {
+        stackOfCards.add(c);
+    }
+    
+    /**
+     * Draw a card from the deck
+     * @return Card drawn*/ 
+    public Card drawCard() {
+        return stackOfCards.remove(stackOfCards.size()-1);
+    }
+    
+    /**
+     *
+     **/ 
+    public void shuffle() {
+        //TODO
+    }
 
 }
