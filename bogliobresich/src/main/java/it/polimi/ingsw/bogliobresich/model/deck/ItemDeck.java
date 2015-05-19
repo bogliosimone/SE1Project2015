85c547889b91d60b4deff3edf719932595be23e5
matteo bresich <matteo.bresich@mail.polimi.it>
2015-05-19 18:10:31 +0200

    Aggiunto ItemDeck

---
+++ bogliobresich/src/main/java/it/polimi/ingsw/bogliobresich/model/deck/ItemDeck.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package it.polimi.ingsw.bogliobresich.model.deck;
+
+
+/**
+ * @author matteobresich
+ * ItemDeck is made ​​up of only item cards
+ */
+public class ItemDeck extends Deck {
+	public ItemDeck() {
+		
+	}
+
+	@Override
+    public void shuffle() {
+	    // TODO Auto-generated method stub
+	    
+    }
+}
