a76d83b2ae7a1771928d2a382b40c03b474ff5fa
matteo bresich <matteo.bresich@mail.polimi.it>
2015-05-19 15:30:52 +0200

    removed contenuto di prova da app

--- bogliobresich/src/main/java/it/polimi/ingsw/bogliobresich/App.java
+++ bogliobresich/src/main/java/it/polimi/ingsw/bogliobresich/App.java
@@ -8,18 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello" );
-        System.out.println("Prova di update");
-        int a = 0;
-        ciao();
-        ciao();
-        ciao();
-    }
-    public static void ciao(){
-    	int a=0;
-    	if(a==0)
-    		System.out.println("ciao");
-    	else 
-    		System.out.println("non va bene");
+    	
     }
 }
