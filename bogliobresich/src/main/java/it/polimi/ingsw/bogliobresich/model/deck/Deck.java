b82bd6c0f4d75fb31bb590f92e3336ba89001879
matteo bresich <matteo.bresich@mail.polimi.it>
2015-05-20 08:54:39 +0200

    Aggiunto isStackOfCardEmpty

--- bogliobresich/src/main/java/it/polimi/ingsw/bogliobresich/model/deck/Deck.java
+++ bogliobresich/src/main/java/it/polimi/ingsw/bogliobresich/model/deck/Deck.java
@@ -14,22 +14,31 @@ import java.util.ArrayList;
  *
  */
 public abstract class Deck {
-    
-    private ArrayList<Card> stackOfCards = new ArrayList<Card>();
-    
-    public abstract void shuffle();
-    
-    /**
-     * Add a card to the deck 
-     * @param c Card: Card to add in the deck*/
-    public void addCard(Card c) {
-        stackOfCards.add(c);
-    }
-    
-    /**
-     * Draw a card from the deck
-     * @return Card drawn*/ 
-    public Card drawCard() {
-        return stackOfCards.remove(stackOfCards.size()-1);
-    }
+
+	private ArrayList<Card> stackOfCards = new ArrayList<Card>();
+
+	public abstract void shuffle();
+
+	/**
+	 * Add a card to the deck 
+	 * @param c Card: Card to add in the deck*/
+	public void addCard(Card c) {
+		stackOfCards.add(c);
+	}
+
+
+	/**
+	 * 
+	 * @return Return true if the stack of cards is empty
+	 */
+	public boolean isStackOfCardEmpty() {
+		return stackOfCards.isEmpty();
+	}
+
+	/**
+	 * Draw a card from the deck
+	 * @return Card drawn*/ 
+	public Card drawCard() {
+		return stackOfCards.remove(stackOfCards.size()-1);
+	}
 }
