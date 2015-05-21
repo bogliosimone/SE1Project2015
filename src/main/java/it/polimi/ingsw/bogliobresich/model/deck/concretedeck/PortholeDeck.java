addea109c158697bafe662e9c97c4f5e80a9a005
matteo bresich <matteo.bresich@mail.polimi.it>
2015-05-21 10:11:21 +0200

    Added PortholeDeck class constructor

--- src/main/java/it/polimi/ingsw/bogliobresich/model/deck/concretedeck/PortholeDeck.java
+++ src/main/java/it/polimi/ingsw/bogliobresich/model/deck/concretedeck/PortholeDeck.java
@@ -3,6 +3,8 @@
  */
 package it.polimi.ingsw.bogliobresich.model.deck.concretedeck;
 
+import it.polimi.ingsw.bogliobresich.model.cards.Card;
+import it.polimi.ingsw.bogliobresich.model.cards.PortholeCard;
 import it.polimi.ingsw.bogliobresich.model.deck.Deck;
 
 /**
@@ -10,5 +12,14 @@ import it.polimi.ingsw.bogliobresich.model.deck.Deck;
  *
  */
 public class PortholeDeck extends Deck {
+	/**
+	 * Class constructor
+	 */
+	public PortholeDeck() {
+		for (int i = 0; i < 6; i++) {
+			Card card = new PortholeCard();
+			super.addCard(card);
+		}
+	}
 
 }
