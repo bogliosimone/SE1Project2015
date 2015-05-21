81e53b183c67a531a6d081e00d9cfb8c1e8a27cd
matteo bresich <matteo.bresich@mail.polimi.it>
2015-05-21 10:11:36 +0200

    Added SectorDeck class constructor

--- src/main/java/it/polimi/ingsw/bogliobresich/model/deck/concretedeck/SectorDeck.java
+++ src/main/java/it/polimi/ingsw/bogliobresich/model/deck/concretedeck/SectorDeck.java
@@ -3,6 +3,8 @@
  */
 package it.polimi.ingsw.bogliobresich.model.deck.concretedeck;
 
+import it.polimi.ingsw.bogliobresich.model.cards.Card;
+import it.polimi.ingsw.bogliobresich.model.cards.SectorCard;
 import it.polimi.ingsw.bogliobresich.model.deck.Deck;
 
 /**
@@ -10,5 +12,15 @@ import it.polimi.ingsw.bogliobresich.model.deck.Deck;
  *
  */
 public class SectorDeck extends Deck {
+	/**
+	 * Class constructor
+	 */
+	public SectorDeck() {
+		for (int i = 0; i < 25; i++) {
+			Card card = new SectorCard();
+			super.addCard(card);
+		}
+		
+	}
 
 }
