726d395852b95b7d9c2c81251f864681aaad528f
matteo bresich <matteo.bresich@mail.polimi.it>
2015-05-21 10:03:28 +0200

    Added PortholeDeck

---
+++ src/main/java/it/polimi/ingsw/bogliobresich/model/deck/concretedeck/PortholeDeck.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package it.polimi.ingsw.bogliobresich.model.deck.concretedeck;
+
+import it.polimi.ingsw.bogliobresich.model.deck.Deck;
+
+/**
+ * @author matteobresich
+ *
+ */
+public class PortholeDeck extends Deck {
+
+}
