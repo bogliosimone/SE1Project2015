0b8dd92447136cae36fabf272120bffe16ba274d
matteo bresich <matteo.bresich@mail.polimi.it>
2015-05-22 09:58:36 +0200

    Added ConstantsCard

---
+++ src/main/java/it/polimi/ingsw/bogliobresich/model/cards/ConstantsCard.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package it.polimi.ingsw.bogliobresich.model.cards;
+
+/**
+ * @author matteobresich
+ *
+ */
+public final class ConstantsCard {
+    static final String ALIEN = "Alien";
+    static final String HUMAN = "Human"; 
+    
+    private ConstantsCard() {
+        //not called
+    }
+}
