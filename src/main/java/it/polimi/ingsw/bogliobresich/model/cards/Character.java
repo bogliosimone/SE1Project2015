1b279123ca9b16354ac55956a195a0377a252367
matteo bresich <matteo.bresich@mail.polimi.it>
2015-05-22 10:02:18 +0200

    Added type in Character with constants

--- src/main/java/it/polimi/ingsw/bogliobresich/model/cards/Character.java
+++ src/main/java/it/polimi/ingsw/bogliobresich/model/cards/Character.java
@@ -8,23 +8,30 @@ package it.polimi.ingsw.bogliobresich.model.cards;
  *
  */
 public enum Character {
-    CAPTAIN("Ennio Maria Dominoni"),
-    PILOT("Julia Niguloti"),
-    PSYCHOLOGIST("Silvano Porpora"),
-    SOLDIER("Tuccio Brendon"),
-    ALIENONE("Piero Ceccarella"),
-    ALIENTWO("Vittorio Martana"),
-    ALIENTHREE("Maria Galbani"),
-    ALIENFOUR("Paolo Landon");
+    CAPTAIN("Ennio Maria Dominoni",ConstantsCard.HUMAN),
+    PILOT("Julia Niguloti",ConstantsCard.HUMAN),
+    PSYCHOLOGIST("Silvano Porpora",ConstantsCard.HUMAN),
+    SOLDIER("Tuccio Brendon",ConstantsCard.HUMAN),
+    ALIENONE("Piero Ceccarella",ConstantsCard.ALIEN),
+    ALIENTWO("Vittorio Martana",ConstantsCard.ALIEN),
+    ALIENTHREE("Maria Galbani",ConstantsCard.ALIEN),
+    ALIENFOUR("Paolo Landon",ConstantsCard.ALIEN);
+    
     
     private String name;
+    private String type;
     
-    Character(String name) {
+    Character(String name, String type) {
         this.name = name;
+        this.type = type;
     }
     
     public String getCharacterName() {
         return this.name;
+    }
+    
+    public String getCharacterType() {
+        return this.type;
     }
 
 }
