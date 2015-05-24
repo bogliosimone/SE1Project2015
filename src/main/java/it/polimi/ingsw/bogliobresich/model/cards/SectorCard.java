bf627b2a59d2dc0cf320ac73046a84322017db0c
matteo bresich <matteo.bresich@mail.polimi.it>
2015-05-24 10:38:06 +0200

    Added isDataOk in SectorCard

--- src/main/java/it/polimi/ingsw/bogliobresich/model/cards/SectorCard.java
+++ src/main/java/it/polimi/ingsw/bogliobresich/model/cards/SectorCard.java
@@ -16,7 +16,7 @@ public class SectorCard extends Card {
      * @param d indicates if you have to draw an item card  
      */
     public SectorCard(String noise, boolean d) {
-        if(true) { //TODO controllo immissione
+        if(isDataOk(noise)) {
             this.drawAnItem = d;
             this.noise = noise;
         }
@@ -29,6 +29,17 @@ public class SectorCard extends Card {
     public boolean isThereNoise() {
         //If there isn't any type of noise in the sector card, the sector card is silence  
         return !this.noise.equals(ConstantsCard.SILENCE);
+    }
+    
+    /**
+     * Return if the data to be entered is correct
+     * @return true if the data to be entered are ok
+     */
+    private boolean isDataOk(String noise) {
+        if(noise.equals(ConstantsCard.SILENCE)||noise.equals(ConstantsCard.NOISE_MY_SECTOR)||noise.equals(ConstantsCard.NOISE_ANY_SECTOR)) {
+            return true;
+        }
+        return false;
     }
 
     @Override
