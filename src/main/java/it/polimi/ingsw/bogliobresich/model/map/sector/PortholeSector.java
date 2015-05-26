da13d1db8d03a4ad0d341676985f496ef0e63e24
Simone Boglio <bogliosimone@gmail.com>
2015-05-26 17:51:16 +0200

    Improve class map with lot of method, clean code and javadoc

--- src/main/java/it/polimi/ingsw/bogliobresich/model/map/sector/PortholeSector.java
+++ src/main/java/it/polimi/ingsw/bogliobresich/model/map/sector/PortholeSector.java
@@ -8,23 +8,31 @@ package it.polimi.ingsw.bogliobresich.model.map.sector;
  * @author simoneboglio
  */
 public class PortholeSector extends Sector {
+    /**
+     * number of porthole
+     */
+    int portholeNumber;
 
     /**
      * Constructor act to create a PortholeSector with letter-number as coordinate
      * @param letter
      * @param number
+     * @param portholeNumber
      */
-    public PortholeSector(char letter, int number){
+    public PortholeSector(char letter, int number, int portholeNumber){
         super(letter,number);
+        this.portholeNumber=portholeNumber;
     }
 
     /**
      * Constructor act to create a PortholeSector with x-y as coordinate
      * @param letter
      * @param number
+     * @param portholeNumber
      */
-    public PortholeSector(int x,int y){
+    public PortholeSector(int x,int y,int portholeNumber){
         super(x,y);
+        this.portholeNumber=portholeNumber;
     }
     
     @Override
@@ -38,5 +46,10 @@ public class PortholeSector extends Sector {
      */
     public void setCrossable(boolean crossable){
         this.isCrossable= crossable;
+    }
+    
+    @Override
+    public String toString(){
+        return new String (this.coordinate.toString()+" phNumb: "+this.portholeNumber+" Crossable = "+this.isCrossable );
     }
 }
