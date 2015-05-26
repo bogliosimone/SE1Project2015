135ca2ec3311ea06fa4a10e641b62e54d5256f93
matteo bresich <matteo.bresich@mail.polimi.it>
2015-05-26 16:44:16 +0200

    Added CharacterDeckTest

---
+++ src/test/java/it/polimi/ingsw/bogliobresich/model/deck/concretedeck/CharacterDeckTest.java
@@ -0,0 +1,61 @@
+package it.polimi.ingsw.bogliobresich.model.deck.concretedeck;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import it.polimi.ingsw.bogliobresich.model.deck.Deck;
+
+public class CharacterDeckTest extends Deck {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testShuffle() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testReShuffle() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testAddCard() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testIsEmpty() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testIsDiscardedCardsEmpty() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testDrawCard() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testDiscardCard() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetReShuffle() {
+        fail("Not yet implemented");
+    }
+
+}
