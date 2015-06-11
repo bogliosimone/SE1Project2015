4c163b7f3180a8f80c4c21a0b8cb2b8445e44346
matteo bresich <matteo.bresich@mail.polimi.it>
2015-06-11 21:50:10 +0200

    resolving conflict

---
+++ src/main/java/it/polimi/ingsw/bogliobresich/controller/Match.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package it.polimi.ingsw.bogliobresich.controller;
+
+import it.polimi.ingsw.bogliobresich.model.Characters;
+import it.polimi.ingsw.bogliobresich.model.cards.Card;
+import it.polimi.ingsw.bogliobresich.model.cards.CharacterCard;
+import it.polimi.ingsw.bogliobresich.model.deck.Deck;
+import it.polimi.ingsw.bogliobresich.model.deck.DeckFactory;
+import it.polimi.ingsw.bogliobresich.model.deck.MyDeckFactory;
+import it.polimi.ingsw.bogliobresich.model.deck.exception.CardFinishedException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author matteobresich
+ *
+ */
+public class Match {
+    public static void main (String args[]) {
+        DeckFactory factory = new MyDeckFactory();
+        Deck characterDeck = factory.createCharacterDeck();
+
+        characterDeck.showCards();
+
+        System.out.println("MESCOLO:");
+        characterDeck.shuffle();
+        characterDeck.showCards();
+        System.out.println("PESCO:");
+        List <Card> cards = new ArrayList();
+        for (int i = 0; i < 8; i++)
+        {
+            try{
+                Card c = characterDeck.drawCard();
+                cards.add(c);
+                System.out.println("Pesco " + c);
+            }
+            catch (CardFinishedException e) { System.out.println("CARTA: " + i + " NON ESISTENTE!");}
+        }  
+        characterDeck.showCards();
+        
+//        System.out.println("SCARTO:");
+//        for (int i = 0; i < 8; i++)
+//        {
+//            Card c = cards.get(i);
+//            characterDeck.discardCard(c);
+//            System.out.println("Scarto " + c);
+//            
+//        }
+//        characterDeck.showCards();
+                
+    }
+    
+
+}
