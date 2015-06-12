7d30092b6767dfdcf11acbd23b193e39c1cd3fda
matteo bresich <matteo.bresich@mail.polimi.it>
2015-06-12 11:01:01 +0200

    Added GameProtocol

---
+++ src/main/java/it/polimi/ingsw/bogliobresich/communication/server/GameProtocol.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package it.polimi.ingsw.bogliobresich.communication.server;
+
+/**
+ * @author matteobresich
+ * 
+ * This Enums contains all the possible net operations supported by the game. 
+ */
+
+public class GameProtocol {
+    
+
+}
