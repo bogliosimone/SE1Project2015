7e47f09ef59d570b7676241b3a7b6619648c0074
Simone Boglio <bogliosimone@gmail.com>
2015-06-15 17:07:13 +0200

    fix constructor match

--- src/main/java/it/polimi/ingsw/bogliobresich/model/notifications/Commands.java
+++ src/main/java/it/polimi/ingsw/bogliobresich/model/notifications/Commands.java
@@ -10,7 +10,7 @@ package it.polimi.ingsw.bogliobresich.model.notifications;
 public enum Commands {
     GENERIC_MESSAGE, //(String) generic string notification for all player
     
-    FATAL_ERROR, //(String) fatal error, something wrong, the game is broken, end it 
+    FATAL_ERROR, //(String) fatal error, something wrong, the game is broken, end it
     GENERIC_ERROR, //(String) maybe you try to do something you can't do in that moment
     
     PLAYER_JOIN_WAIT_ROOM, // when user join wait room
@@ -18,6 +18,8 @@ public enum Commands {
     WHO_ARE_YOU, //(Player) player know is role in the game
     LIST_USERS, //(List User) player know his game mate
     GAME_START, // the game begin
+    
+    
     
     
     
