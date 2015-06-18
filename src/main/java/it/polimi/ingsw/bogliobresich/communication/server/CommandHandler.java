50b4e3368187bf0750a36cb1d6c05b179cfd860f
matteo bresich <matteo.bresich@mail.polimi.it>
2015-06-18 11:39:58 +0200

    Added some clients command

--- src/main/java/it/polimi/ingsw/bogliobresich/communication/server/CommandHandler.java
+++ src/main/java/it/polimi/ingsw/bogliobresich/communication/server/CommandHandler.java
@@ -5,11 +5,13 @@ package it.polimi.ingsw.bogliobresich.communication.server;
 
 import it.polimi.ingsw.bogliobresich.communication.ClientCommand;
 import it.polimi.ingsw.bogliobresich.communication.CommandType;
-import it.polimi.ingsw.bogliobresich.model.map.Coordinate;
 import it.polimi.ingsw.bogliobresich.model.match.action.AttackAction;
+import it.polimi.ingsw.bogliobresich.model.match.action.DiscardAction;
 import it.polimi.ingsw.bogliobresich.model.match.action.DrawSectorAction;
+import it.polimi.ingsw.bogliobresich.model.match.action.EndPhaseAction;
 import it.polimi.ingsw.bogliobresich.model.match.action.EndTurnAction;
 import it.polimi.ingsw.bogliobresich.model.match.action.MovementAction;
+import it.polimi.ingsw.bogliobresich.model.match.action.PlayItemAction;
 import it.polimi.ingsw.bogliobresich.model.match.action.RumorCoordinate;
 import it.polimi.ingsw.bogliobresich.model.player.Player;
 
@@ -23,26 +25,36 @@ public class CommandHandler {
         CommandType cmdType = command.getCommandType();
         switch(cmdType) {
         case DO_MOVE_REQUEST: 
-            matchHandler.executeAction(player, new MovementAction(command.getCoordinate()));
+            if(command.getCoordinate() != null) {
+                matchHandler.executeAction(player, new MovementAction(command.getCoordinate()));
+            }
         break;
         case DO_ATTACK_REQUEST:
             matchHandler.executeAction(player, new AttackAction());
             break;
         case DO_DISCARD_ITEM_REQUEST:
-            //matchHandler.executeAction(player, new DiscardAction(command.getCard()));
+            if(command.getCard() != null) { 
+                matchHandler.executeAction(player, new DiscardAction(command.getCard()));
+            }
             break;
         case DO_DRAW_SECTOR_REQUEST:
             matchHandler.executeAction(player, new DrawSectorAction());
             break;
         case DO_END_MOVEMENT_PHASE_REQUEST:
+            matchHandler.executeAction(player, new EndPhaseAction());
             break;
         case DO_END_TURN_REQUEST:
             matchHandler.executeAction(player, new EndTurnAction());
             break;
         case DO_PLAY_ITEM_REQUEST:
+            if(command.getCard() != null) { 
+                matchHandler.executeAction(player, new PlayItemAction(command.getCard()));
+            }
             break;
         case DO_RUMOR_IN_COORDINATE_REQUEST:
-            matchHandler.executeAction(player, new RumorCoordinate(command.getCoordinate()));
+            if(command.getCoordinate() != null) {
+                matchHandler.executeAction(player, new RumorCoordinate(command.getCoordinate()));
+            }
             break;
         default:
             break;
