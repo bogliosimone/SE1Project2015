2634b0231484b9dad5966dfae37997e634f69ff1
matteo bresich <matteo.bresich@mail.polimi.it>
2015-06-20 17:55:44 +0200

    Added FilePaths

---
+++ src/main/java/it/polimi/ingsw/bogliobresich/FilePaths.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package it.polimi.ingsw.bogliobresich;
+
+/**
+ * @author matteobresich
+ *
+ */
+public class FilePaths {
+    
+    //CARDS
+    public static final String ITEM_CARD_ICON = "images/cards/itemcard_icon.png";
+    
+    public static final String ITEM_CARD_ADRENALINE = "images/cards/adrenaline_icon.png";
+    public static final String ITEM_CARD_ATTACK = "images/cards/attack_icon.png";
+    public static final String ITEM_CARD_DEFENSE = "images/cards/defense_icon.png";
+    public static final String ITEM_CARD_SEDATIVES = "images/cards/sedatives_icon.png";
+    public static final String ITEM_CARD_SPOTLIGHT = "images/cards/spotlight_icon.png";
+    public static final String ITEM_CARD_TELEPORT = "images/cards/teleport_icon.png";
+
+    //PLAYERS
+    public static final String CAPTAIN_ICON = "images/players/the_captain_icon.png";
+    public static final String PILOT_ICON = "images/players/the_pilot_icon.png";
+    public static final String PSYCHOLOGIST_ICON = "images/players/the_psychologist_icon.png";
+    public static final String SOLDIER_ICON = "images/players/the_soldier_icon.png";
+    
+    public static final String FIRST_ALIEN_ICON = "images/players/the_first_alien_icon.png";
+    public static final String SECOND_ALIEN_ICON = "images/players/the_second_alien_icon.png";
+    public static final String THIRD_ALIEN_ICON = "images/players/the_third_alien_icon.png";
+    public static final String FOURTH_ALIEN_ICON = "images/players/the_fourth_alien_icon.png";
+    
+    //PORTHOLE
+    public static final String PORTHOLE_GREEN = "images/portholes/green_porthole_icon.png";
+    public static final String PORTHOLE_RED = "images/portholes/red_porthole_icon.png";
+    
+    /***/
+    private FilePaths () {}
+}
