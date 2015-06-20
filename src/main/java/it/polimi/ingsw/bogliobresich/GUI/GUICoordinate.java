46096b729c338f16c3e2975e4d8ff5d83085e162
Simone Boglio <bogliosimone@gmail.com>
2015-06-20 18:47:57 +0200

    Finally work gui for hex map

---
+++ src/main/java/it/polimi/ingsw/bogliobresich/GUI/GUICoordinate.java
@@ -0,0 +1,84 @@
+/**
+ * 
+ */
+package it.polimi.ingsw.bogliobresich.GUI;
+
+import java.awt.Color;
+
+import it.polimi.ingsw.bogliobresich.model.map.Coordinate;
+
+/**
+ * @author simoneboglio
+ */
+public class GUICoordinate {
+    Coordinate coord;
+    Color defaultColour;
+    Color actualColour;
+    char sectorType;
+    String stringValue;
+    
+    public GUICoordinate(){
+        
+    }
+    
+    public GUICoordinate(int x, int y, char sectorType, Color color){
+        this.coord=new Coordinate(x,y);
+        this.sectorType=sectorType;
+        this.defaultColour=color;
+        this.actualColour=color;
+        if(sectorType=='1'||sectorType=='2'||sectorType=='3'||sectorType=='4')
+                this.stringValue=Character.toString(sectorType);
+        else 
+            this.stringValue=Character.toString((char)(coord.getLetter()+1))+(coord.getNumber()+1);
+        
+    }
+    
+    public int getX(){
+        return coord.getX();
+    }
+    
+    public int getY(){
+        return coord.getY();
+    }
+    
+    public void setCoord(Coordinate coord) {
+        this.coord = coord;
+    }
+    public Color getDefaultColour() {
+        return defaultColour;
+    }
+    
+    
+    public void resetColour(){
+        actualColour=defaultColour;
+    }
+    
+    public Color getActualColour() {
+        return actualColour;
+    }
+    public void setActualColour(Color color) {
+        this.actualColour = color;
+    }
+    public char getSectorType() {
+        return sectorType;
+    }
+    public void setSectorType(char sectorType) {
+        this.sectorType = sectorType;
+    }
+    public String getTextLabel(){
+        return this.coord.toString();
+    }
+
+    public String getStringValue() {
+        return stringValue;
+    }
+
+    public void setStringValue(String stringValue) {
+        this.stringValue = stringValue;
+    }
+    
+    @Override
+    public String toString(){
+        return this.getX()+"x"+this.getY()+" value: "+stringValue;
+    }
+}
--- src/main/java/it/polimi/ingsw/bogliobresich/model/map/ConstantMap.java
+++ src/main/java/it/polimi/ingsw/bogliobresich/model/map/ConstantMap.java
@@ -11,15 +11,15 @@ public class ConstantMap {
     /**
      * rows of the standard map
      */
-    static final int ROWMAP=14;
+    public static final int ROWMAP=14;
     /**
      * columns of the standard map
      */
-    static final int COLUMNMAP=23;
+    public static final int COLUMNMAP=23;
     /**
      * path of the standard map.txt file
      */
-    static final String NAMEFILEMAP= new String ("src/main/resources/galilei.txt");
+    public static final String NAMEFILEMAP= new String ("src/main/resources/galilei.txt");
 
     /**
      * private constructor for a class of constant
